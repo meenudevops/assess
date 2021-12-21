b2b014526e8395d3485d689eb485f09f36d33fc0
meenudevops <meenu.meenu95@gmail.com>
2021-12-21 09:23:03 +0530

    path changed

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "C:/Users/meenu.m.nair/Downloads/southern-splice-330005-c06109f1c7f8.json"
+  credentials = "southern-splice-330005-c06109f1c7f8.json"
   project     = var.project
   region      = var.region
 }
