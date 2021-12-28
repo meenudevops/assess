c482d83f6a6d10299dac7928aeae0dc7e2164017
meenudevops <meenu.meenu95@gmail.com>
2021-12-28 12:48:40 +0530

    bucket added

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider google {
+    credentials = "southern-splice-330005-77c973808679.json"
+    project     = "southern-splice-330005"
+}
+
+resource "google_storage_bucket" "my_new_bucket" {
+  name     = "new-bucket1"
+  location = "us-central1"
+}
