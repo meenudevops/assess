836a6d30d8e161cd6349b2bd5bb2bc77f9dd3904
meenudevops <71659374+meenudevops@users.noreply.github.com>
2021-12-28 12:18:36 +0530

    Delete subnet1.tf

--- modules/subnet1.tf
+++
@@ -1,8 +0,0 @@
-resource "google_compute_subnetwork" "subnet1" {
- name         = "subnet1"
-ip_cidr_range = "10.0.0.0/24"
-region        = "us-central1"
- depends_on   = [google_compute_network.vpc_network]
- network      = google_compute_network.vpc_network.id
-
-}
