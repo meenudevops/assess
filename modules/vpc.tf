d55fbb26f9fd61a70e580f82a2d7c3f4e9caad33
meenudevops <meenu.meenu95@gmail.com>
2021-12-29 09:50:09 +0530

    new commit

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -1,6 +1,5 @@
 resource "google_compute_network" "vpc_network" {
     name                    = "newnet"
     auto_create_subnetworks = false
-    mtu                     = 1460
-   
+    mtu                     = 1460 
 }
