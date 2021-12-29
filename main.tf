2d6a851c5298a105f00fb1db6074e0ddebbe4e82
meenudevops <meenu.meenu95@gmail.com>
2021-12-29 09:30:34 +0530

    name updated

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "google" {
   credentials = "southern-splice-330005-77c973808679.json"
   project     = "southern-splice-330005"
 }
-resource "google_storage_bucket" "mygcsbucket" {
+resource "google_storage_bucket" "mygcsbucket010" {
   name          = "new-bucket1"
   location      = "US"
   force_destroy = true
