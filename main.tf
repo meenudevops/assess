2739bcc0880a11c00d03a323189285157f5f5bc4
meenudevops <meenu.meenu95@gmail.com>
2021-12-29 09:24:29 +0530

    name updated

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "google" {
   credentials = "southern-splice-330005-77c973808679.json"
   project     = "southern-splice-330005"
 }
-resource "google_storage_bucket" "my_bucket" {
+resource "google_storage_bucket" "my_sample_terra_bucket" {
   name          = "new-bucket1"
   location      = "US"
   force_destroy = true
