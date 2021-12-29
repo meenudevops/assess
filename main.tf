33bc6e95547cdcc801bdcc6769c5c33987276c3f
meenudevops <meenu.meenu95@gmail.com>
2021-12-29 09:14:44 +0530

    commit2

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
-provider google {
-    credentials = "southern-splice-330005-77c973808679.json"
-    project     = "southern-splice-330005"
+provider "google" {
+  credentials = "southern-splice-330005-77c973808679.json"
+  project     = "southern-splice-330005"
 }
-
 resource "google_storage_bucket" "my_new_bucket" {
   name     = "new-bucket1"
   location = "us-central1"
